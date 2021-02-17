6ddb50a4867d3fa352b6b05726989eb4c2d3ecb3
Чесников <maxim.chesnikov@chulakov.ru>
2021-02-17 21:55:20 +0300

    Initial Commit

---
+++ TestWeatherAppSwiftUI/TestWeatherAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestWeatherAppSwiftUIApp.swift
+//  TestWeatherAppSwiftUI
+//
+//  Created by Max Chesnikov on 17.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestWeatherAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
